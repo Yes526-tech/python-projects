6fe298c3e52418d4243f708314b8f2ae0911efa9
Yes526-tech <ysert20051@gmail.com>
2021-06-08 17:32:30 +0300

    day in month added

--- caesar_project/main.py
+++ caesar_project/main.py
@@ -1,5 +1,11 @@
+import string
+
 from art import logo
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = []
+alphabet_letters =string.ascii_lowercase
+for letter in alphabet_letters:
+    alphabet += letter
+alphabet = alphabet * 2
 wanna_continue = True
 print(logo)
 while wanna_continue:
