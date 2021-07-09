f43cb6dfd22279d9314e9dace14c5f4d8b5f2152
Yes526-tech <ysert20051@gmail.com>
2021-07-09 15:37:12 +0300

    circle

---
+++ new_format_of_me/quiz game with class/quiz_game.py
@@ -0,0 +1,4 @@
+question_1 = input("türkiyenin başkenti istanbuldur D/Y > ").lower()
+class Checking():
+    def check_the_answer(self, question):
+        if question == "true" and :
---
+++ new_format_of_me/çevre alan/daire.py
@@ -0,0 +1,15 @@
+
+radius = float(input("enter your circle's radius: "))
+
+pi = 3.14
+perimeter = 2 * pi * radius ** 2
+field = pi * radius ** 2
+
+choice = input("perimeter or field of circle: ").lower()
+
+if choice == "field":
+    print(field)
+elif choice == "perimeter":
+    print(perimeter)
+else:
+    print("you enter wrong entry. Try again.")
