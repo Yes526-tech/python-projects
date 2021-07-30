54e4fda3ccc3467d5c1741032b98a8789e012c1d
Yes526-tech <ysert20051@gmail.com>
2021-07-30 13:50:30 +0300

    json

--- project_python/json/main.py
+++ project_python/json/main.py
@@ -1,32 +1,62 @@
 # deserialize
-import json
+# import json
 
 # with open("person.json") as file:
 #     #    data = file.read()
 #     data = json.load(file)
 # print(data["first_name"])
+#
+# data = """
+#     {
+#   "first_name": "Yunus",
+#   "last_name": "Sert",
+#   "hobbies": [
+#     "basketball",
+#     "programming"
+#   ],
+#   "age": 16,
+#   "siblings": [
+#     {
+#       "first_name": "esra",
+#       "age": 13
+#     },
+#     {
+#       "first_name": "Kadir",
+#       "age": 6
+#     }
+#   ]
+# }
+# """
+# data = json.loads(data)
+# print(data)
+# print(type(data))
 
-data = """
-    {
-  "first_name": "Yunus",
-  "last_name": "Sert",
-  "hobbies": [
-    "basketball",
-    "programming"
-  ],
-  "age": 16,
-  "siblings": [
-    {
-      "first_name": "esra",
-      "age": 13
-    },
-    {
-      "first_name": "Kadir",
-      "age": 6
-    }
-  ]
+# serialize
+import json
+person = {
+    "first_name": "Yunus",
+    "last_name": "Sert",
+    "hobbies": [
+        "basketball",
+        "programming"
+    ],
+    "age": 16,
+    "siblings": [
+        {
+            "first_name": "esra",
+            "age": 13
+        },
+        {
+            "first_name": "Kadir",
+            "age": 6
+        }
+    ]
 }
-"""
-data = json.loads(data)
-print(data)
-print(type(data))
+# print(person)
+# print(person["first_name"])
+# result = json.dumps(person, ensure_ascii=False,indent=5)
+# print(result)
+# print(type(result))
+
+with open("person.json","w") as file:
+    json.dump(person, file)
